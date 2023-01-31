59e20caf37c086558eba1776f81b6af2f56567f3
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-01-31 14:56:48 -0600

    added thoughts and routes

---
+++ messages/messages.js
@@ -0,0 +1,10 @@
+const errors = {
+    USER_NOT_FOUND:'USER NOT FOUND',
+    NO_IDEA:'No idea with this id!',
+    NO_USER_FOR_IDEA:'idea created but no user with this id!'
+}
+const success = {
+    USER_DELETED_SUCCESSFULLY:'USER DELETED SUCCESSFULLY',
+    IDEA_DELETED_SUCCESSFULLY:'idea successfully deleted!',
+    IDEA_CREATED_SUCCESSFULLY:'idea successfully created!',
+}
